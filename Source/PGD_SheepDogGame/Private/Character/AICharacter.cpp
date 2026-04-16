60a972daff5cfc9b9551ec453ced125de4b4d193
Draketoons <mattadragon5@gmail.com>
2026-04-16 17:50:02 -0500

    Started on the basic AI system

---
+++ Source/PGD_SheepDogGame/Private/Character/AICharacter.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AI/AC_Sheep.h"
+#include "DrawDebugHelpers.h"
+#include "Character/AICharacter.h"
+
+// Sets default values
+AAICharacter::AAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	AIController = Cast<AAC_Sheep>(GetController());
+}
+
+// Called every frame
+void AAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	DrawDebugSphere(GetWorld(), GetActorLocation(), DetectionRadius, 10, FColor::Red, false, -1.0f, 1.0, 1.0);
+
+	float playerDistance = FVector::Dist(GetActorLocation(), Player->GetActorLocation());
+
+	if (playerDistance < DetectionRadius)
+	{
+		AvoidPlayer();
+	}
+
+}
+
+// Called to bind functionality to input
+void AAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AAICharacter::AvoidPlayer()
+{
+	if (Player)
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("AIController Location: %f, %f, %f"), GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z)
+		FVector playerDirection = Player->GetActorLocation() - GetActorLocation();
+
+		FVector targetLocation = -playerDirection * AvoidOffset;
+
+		DrawDebugSphere(GetWorld(), targetLocation * AvoidOffset, 50, 10, FColor::Green, false, -1.0f, 1.0, 1.0);
+
+		AIController->MoveToLocation(targetLocation * AvoidOffset);
+	}
+}
+
---
+++ Source/PGD_SheepDogGame/Private/Character/AICharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GameFramework/Character.h"
+#include "AICharacter.generated.h"
+
+UCLASS()
+class AAICharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAICharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	ACharacter* Player;
+
+	UPROPERTY()
+	AAIController* AIController;
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float DetectionRadius;
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float AvoidOffset;
+
+	void AvoidPlayer();
+
+};
